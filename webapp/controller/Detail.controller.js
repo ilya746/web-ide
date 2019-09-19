62d19e9c9743cc3cdbeb38113285fe6cc582e3d1
ilya746 <krutikov_ilya@mail.ru>
2019-09-19 13:52:33 +0000

    add routing parameters

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -7,19 +7,30 @@ sap.ui.define([
 		onInit: function () {
 		
 		},
+		
+		onSelectionChange: function(oEvent) {
+			
+ 		var oTable = oEvent.getSource().getSelectedItem().getBindingContext("detail");
+  var Row = oTable.oModel.getProperty(oTable.sPath);
+			
+			
+		/*	var titleName = oEvent.getSource().getBindingContext().getProperty("carName");
+			var BrandName = oEvent.getSource().getBindingContext().getProperty("Brand");
+			var ModelName = oEvent.getSource().getBindingContext().getProperty("Model");
+			var Power = oEvent.getSource().getBindingContext().getProperty("Power");
+			var data = { carName : titleName, Brand: BrandName, Model: ModelName, Power: Power};*/
+	var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+	oRouter.navTo("carDetail", { Value: JSON.stringify(Row)
+	});
+		},
+		
 		/*
 		переход на предыдущую страницу
 		*/
 		ToMainPage: function () {
 		history.go(-1);
-		},
-		
-			onSelectionChange: function(oEvent) {
-			var sProductId = oEvent.getSource().getBindingContext().getProperty("carID");
-			this.getOwnerComponent().getRouter()
-				.navTo("carDetail",
-					{orderId:this._orderId, productId: sProductId});
 		}
+		
 	});
 
 });
