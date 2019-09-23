d75afd235da9ca27531acf301905c2f607605dff
ilya746 <krutikov_ilya@mail.ru>
2019-09-23 05:18:26 +0000

    complete all CarDetail.view

--- webapp/controller/CarDetail.controller.js
+++ webapp/controller/CarDetail.controller.js
@@ -9,15 +9,13 @@ sap.ui.define([
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			oRouter.getRoute("carDetail").attachPatternMatched(this.onObjectMatched, this);
 			},
-		
 	
 		onObjectMatched: function (oEvent) {
-			var data = JSON.parse(oEvent.getParameter("arguments").Value);
+			var data = JSON.parse(decodeURIComponent(oEvent.getParameter("arguments").Value));  // decodeURIComponent - декодируем данные, которые были переданы на этот view
 			var oModel = new sap.ui.model.json.JSONModel();
 			oModel.setData(data);
 			this.getView().setModel(oModel, "Value");
-			this.getView().bindElement({
-				path: oEvent.getParameter("arguments").Value, model: "carDetail"});
+			this.getView().bindElement({path:oEvent.getParameter("arguments").Value, model: "carDetail"});
 		},
 			
 	
--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -8,8 +8,8 @@ sap.ui.define([
 		
 		},
 		
+		// передача параметров в carDetail.view
 	onSelectionChange: function(oEvent) {
-	
 			var NameValue = oEvent.getSource().getBindingContext().getProperty("carName");
 			var BranValue = oEvent.getSource().getBindingContext().getProperty("Brand");
 			var ModelValue = oEvent.getSource().getBindingContext().getProperty("Model");
@@ -17,18 +17,16 @@ sap.ui.define([
 			var ColorValue = oEvent.getSource().getBindingContext().getProperty("Color");
 			var DescriptionValue = oEvent.getSource().getBindingContext().getProperty("Description");
 			var PriceValue = oEvent.getSource().getBindingContext().getProperty("Price");
-			//var PictureValue = oEvent.getSource().getBindingContext().getProperty("Picture");
-			var data = { carName : NameValue, Brand : BranValue, Model : ModelValue, Power : PowerValue, Color : ColorValue, Description : DescriptionValue, Price : PriceValue};
+			var PictureValue = encodeURIComponent(oEvent.getSource().getBindingContext().getProperty("Picture")); // encodeURIComponent - кодируем символ слэша в url картинок
+			var data = { carName : NameValue, Brand : BranValue, Model : ModelValue, Power : PowerValue, Color : ColorValue, Description : DescriptionValue, Price : PriceValue, Picture:PictureValue};
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			oRouter.navTo("carDetail", { Value: JSON.stringify(data)});
-			
-			
 		},
 		
 
-		/*
-		переход на предыдущую страницу
-		*/
+		
+	//	переход на предыдущую страницу
+	
 		ToMainPage: function () {
 		history.go(-1);
 		}
