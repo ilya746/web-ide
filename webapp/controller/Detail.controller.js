0e4bc621d95ded9ee08e60fc83292cc1b8082ee6
ilya746 <krutikov_ilya@mail.ru>
2019-09-24 05:57:42 +0000

    carDetail.view without display and change fragments. Beauty code

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,15 +1,15 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller"
-], function (Controller,JSONModel) {
+], function (Controller, JSONModel) {
 	"use strict";
 
 	return Controller.extend("Project.Project.controller.Detail", {
 		onInit: function () {
-		
+
 		},
-		
+
 		// передача параметров в carDetail.view
-	onSelectionChange: function(oEvent) {
+		onSelectionChange: function (oEvent) {
 			var NameValue = oEvent.getSource().getBindingContext().getProperty("carName");
 			var BranValue = oEvent.getSource().getBindingContext().getProperty("Brand");
 			var ModelValue = oEvent.getSource().getBindingContext().getProperty("Model");
@@ -18,19 +18,28 @@ sap.ui.define([
 			var DescriptionValue = oEvent.getSource().getBindingContext().getProperty("Description");
 			var PriceValue = oEvent.getSource().getBindingContext().getProperty("Price");
 			var PictureValue = encodeURIComponent(oEvent.getSource().getBindingContext().getProperty("Picture")); // encodeURIComponent - кодируем символ слэша в url картинок
-			var data = { carName : NameValue, Brand : BranValue, Model : ModelValue, Power : PowerValue, Color : ColorValue, Description : DescriptionValue, Price : PriceValue, Picture:PictureValue};
+			var data = {
+				carName: NameValue,
+				Brand: BranValue,
+				Model: ModelValue,
+				Power: PowerValue,
+				Color: ColorValue,
+				Description: DescriptionValue,
+				Price: PriceValue,
+				Picture: PictureValue
+			};
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-			oRouter.navTo("carDetailPage", { Value: JSON.stringify(data)});
+			oRouter.navTo("carDetail", {
+				Value: JSON.stringify(data)
+			});
 		},
-		
 
-		
-	//	переход на предыдущую страницу
-	
+		//	переход на предыдущую страницу
+
 		ToMainPage: function () {
-		history.go(-1);
+			history.go(-1);
 		}
-		
+
 	});
 
 });
